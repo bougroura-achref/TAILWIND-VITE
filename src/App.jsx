aa8433d0abbf751349508fba8e07a196d643a9b9
bougroura-achref <na_bougroura@esi.dz>
2025-02-07 00:22:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-red-500'>hello world</h1> 
+      <h2 className=" underline border-2 ">AChref</h2>
+    </>
+    )
+}
+
+export default App
